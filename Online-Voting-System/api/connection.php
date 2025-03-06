f7fce57a37d2455279ffc2a084712adf7960581c
GOPISETTI GOPI <95520388+gopisettigopi123@users.noreply.github.com>
2025-03-06 17:37:05 +0530

    Add files via upload

---
+++ Online-Voting-System/api/connection.php
@@ -0,0 +1,11 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "online-voting-system");
+
+?>
+
+
+<!-- Author By: MH RONY
+Author Website: https://developerrony.com
+Github Link: https://github.com/dev-mhrony
+Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+-->
